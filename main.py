613ae727e79a6bb8fba8518e296c7ab2b8e97fb6
Sofia Kabir <sofia23kabir@gmail.com>
2024-07-18 19:50:22 +1000

    Moved main.py and __init__.py to src directory and removed from root

--- main.py
+++
@@ -1,9 +0,0 @@
-# connect intialised modules/libraies to the flask application 
-from flask import Flask 
-from init import db, ma, bcrypt, jwt 
-
-#application factories 
-def create_app(): 
-    app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
