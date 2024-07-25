bfe6f5ca195f3d871d83e2b975fd4012f0ca36a0
Sofia Kabir <sofia23kabir@gmail.com>
2024-07-25 14:36:52 +1000

    Updated Task model to include foreign key relationship with Category model

--- src/models/task.py
+++ src/models/task.py
@@ -1,6 +1,8 @@
 from init import db, ma 
 from marshmallow import fields 
 
+# EDIT, ADD COMMENTS AND ERRORS!!
+
 class Task(db.Model): 
     __tablename__ = "tasks"
 
@@ -10,22 +12,30 @@ class Task(db.Model):
     due_date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
-    category = db.Column(db.String)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     user = db.relationship("User", back_populates="tasks")
     comments = db.relationship("Comment", back_populates="task", cascade="all, delete")
+    category = db.relationship("Category", back_populates="tasks") 
 
 class TaskSchema(ma.Schema): 
-
     user = fields.Nested('UserSchema', only=("name", "email"))
-    comments = fields.Nested("CommentSchema", exlcude=["task"])
+    comments = fields.List(fields.Nested("CommentSchema", exclude=["task"]))
+    category = fields.Nested("CategorySchema", only=("label",))
 
     class Meta:
-        fields = ("id","title", "description", "due_date", "status", "priority", "user", "comments")
+        fields = ("id","title", "description", "due_date", "status", "priority", "user", "comments", "category")
         ordered = True 
 
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
+
+
+    
+    
+    
+
+
 
