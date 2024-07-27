c8cbac2b158ca41b8b0e5a7191c3a6b99eafda45
Sofia Kabir <sofia23kabir@gmail.com>
2024-07-27 14:47:43 +1000

    Add docstrings to category model

--- src/models/category.py
+++ src/models/category.py
@@ -1,9 +1,16 @@
 from init import db, ma
 from marshmallow import fields
 
-#ADD ERROR AND COMMENTS!!!
-
 class Category(db.Model):
+    """Defines the Category model with the following attributes:
+    
+    Attributes:
+        id (int): Primary key, auto-incremented.
+        label (str): The name of the category.
+    
+    Relationships:
+        tasks: One-to-many relationship with the Task model.
+    """
     __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +19,8 @@ class Category(db.Model):
     tasks = db.relationship("Task", back_populates='category', cascade='all, delete')
 
 class CategorySchema(ma.Schema):
+    """Schema for serializing and deserializing Category objects."""
+
     tasks = fields.Nested('TaskSchema', many=True, exclude=['category'])
 
     class Meta:
